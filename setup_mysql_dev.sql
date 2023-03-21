7c17d90365a0a9b276566f686e1797fd551a8835
Pretty Pandey <prettypandeypearl@gmail.com>
2023-03-21 12:45:11 -0700

    making adjustments to setup_mysql_dev.sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,10 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 -- Set password for user
-SET PASSSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 -- Grant privileges to user on database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant select privileges to user on performance_schema database
-GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- Flush Privileges
 FLUSH PRIVILEGES;
