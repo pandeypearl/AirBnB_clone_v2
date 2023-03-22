70bf52e2894918c906eaa91277d3588d71ff68ed
Pretty Pandey <prettypandeypearl@gmail.com>
2023-03-22 02:15:19 -0700

    code fix user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship, backref
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
@@ -12,15 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship(
-        "Place",
-        cascade="all,delete",
-        backref=backref("user", cascade="all,delete"),
-        passive_deletes=True,
-        single_parent=True)
-    reviews = relationship(
-        "Review",
-        cascade="all,delete",
-        backref=backref("user", cascade="all,delete"),
-        passive_deletes=True,
-        single_parent=True)
+    places = relationship("Place", cascade="all,delete", backref='user')
+    reviews = relationship("Review", cascade="all,delete", backref='user')
