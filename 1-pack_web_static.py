a95a1031b908f031e1b92dd62793c272883e155e
Pretty Pandey <prettypandeypearl@gmail.com>
2023-04-09 17:48:16 +0000

    updating Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,14 +6,14 @@ import os
 
 
 def do_pack():
-	""" Creates a .tgz archive """
-	date = datetime.now().strftime("%Y%m%d%H%M%S")
-	filename = "versions/web_static_{}.tgz".format(date)
-	if not os.path.exists("versions/"):
-		os.mkdir("versions/")
-	with tarfile.open(filename, "w:gz") as tar:
-		tar.add("web_static", arcname=os.path.basename("web_static"))
-	if os.path.exists(filename):
-		return filename
-	else:
-		return None
+    """ Creates a .tgz archive """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    if not os.path.exists("versions/"):
+        os.mkdir("versions/")
+    with tarfile.open(filename, "w:gz") as tar:
+        tar.add("web_static", arcname=os.path.basename("web_static"))
+    if os.path.exists(filename):
+        return filename
+    else:
+        return None
